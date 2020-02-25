db6c69315f3d1090083b5ce2d07da518f3fc9e62
rannumianda <project@athaya-bpp.com>
2020-02-25 09:33:55 +0800

    penambahan penjumlahan, variabel dan Alert

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var a = 10;
+var a = 10,
+    b = 3;
+var c = a + b;
 console.log("hello World");
 console.log('nilai variabel a adalah ' + a);
 alert("page ini bekerja dalam console dan script.js");
+alert("hasil dari penjumlahan " + a + " + " + b + " = " + c);
