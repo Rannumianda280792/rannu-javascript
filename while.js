c8bd7a20fd8079acc752a80d012426e35fbdddd7
rannumianda <project@athaya-bpp.com>
2020-02-25 16:44:42 +0800

    tambah kondisi while kondisi dan bolean

---
+++ while.js
@@ -0,0 +1,17 @@
+var tulisan = prompt('masukan tulisan yang di inginkan : '),
+    jumlahcetak = prompt('masukan jumlah yang akan di tampilkan : '),
+    cetak = confirm('cetak tulisan ' + tulisan + ' sebanyak ' + jumlahcetak + '?');
+alert('tulisan telah dicetak pada console');
+
+var temp = 1;
+while (temp <= jumlahcetak) {
+    console.log(tulisan + ' ke ' + temp);
+    temp = 1 + temp;
+
+}
+console.log('=================');
+var ulang = true;
+while (ulang) {
+    console.log('Hello World!!');
+    ulang = confirm('ulangi kembali?');
+}
