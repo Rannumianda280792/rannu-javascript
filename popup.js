be26d2ee94f873e2203063043c0aa10f10d68ba1
rannumianda <project@athaya-bpp.com>
2020-02-25 10:26:15 +0800

    tambah kalimat pada alert

--- popup.js
+++ popup.js
@@ -3,5 +3,5 @@ alert('nama saya');
 alert('Rannu Mianda');
 
 var alamat = prompt("Masukan Alamat anda : ");
-alert(alamat);
+alert("alamat anda" + alamat);
 console.log("alamat yang dimasukan adalah " + alamat);
