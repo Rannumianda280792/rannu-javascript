8fee490d1a041176e5f7ff5dcc39d83874678dbf
rannumianda <project@athaya-bpp.com>
2020-02-25 10:16:28 +0800

    tambah prompt dan console log

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
 alert('hello world');
 alert('nama saya');
 alert('Rannu Mianda');
+
+var alamat = prompt("Masukan Alamat anda : ");
+alert(alamat);
+console.log("alamat yang dimasukan adalah " + alamat);
