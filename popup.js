48bd6c5c5607c5cb3e42bf230a9d128efe1c1f11
rannumianda <project@athaya-bpp.com>
2020-02-25 13:12:54 +0800

    tambah popup alert menggunakan while

--- popup.js
+++ popup.js
@@ -3,5 +3,16 @@ alert('nama saya');
 alert('Rannu Mianda');
 
 var alamat = prompt("Masukan Alamat anda : ");
-alert("alamat anda" + alamat);
-console.log("alamat yang dimasukan adalah " + alamat);
+alert("alamat anda " + alamat);
+console.log("sebelum di revisi alamat yang dimasukan adalah " + alamat);
+
+var lagi = confirm('mohon lengkapi alamat ' + alamat + ' anda, tekan OK untuk perubahan');
+
+while (lagi) {
+    alamat = prompt('isikan kembali alamat anda : ');
+    alert('terimakasih, data telah dirubah, cek di console');
+    console.log("setalah di revisi,alamat yang dimasukan adalah " + alamat);
+    lagi = confirm('lakukan kembali inputan?');
+}
+alert('terimakasih');
+console.log("tidak ada perubahan pada alamat");
