b0208f38810224457d00191fd61a0e3563c9be25
rannumianda <project@athaya-bpp.com>
2020-02-25 09:40:31 +0800

    hilangkan var variabel C

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a = 10,
-    b = 3;
-var c = a + b;
+    b = 3,
+    c = a + b;
 console.log("hello World");
 console.log('nilai variabel a adalah ' + a);
 alert("page ini bekerja dalam console dan script.js");
