388891cdd67b51db1d6c58b7d5513889aba624ff
rannumianda <project@athaya-bpp.com>
2020-02-26 17:04:13 +0800

    tambah lopping while dalam for

---
+++ angkot2.js
@@ -0,0 +1,15 @@
+
+for (var jumlahangkot = 1; jumlahangkot <= 1; jumlahangkot++) {
+    var baik = 1;
+    while (baik <= 6) {
+        console.log("angkot No. " + baik + " beroperasi dengan baik");
+        baik++;
+    }
+    var tdkbaik = 7;
+    while (tdkbaik <= 10) {
+        console.log("angkot No. " + tdkbaik + " sedang tidak beroperasi");
+        tdkbaik++;
+
+    }
+
+}
