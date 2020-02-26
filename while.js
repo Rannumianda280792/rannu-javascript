b069e5a52be7b53e442145b04344736d2cb158e5
rannumianda <project@athaya-bpp.com>
2020-02-26 09:42:28 +0800

    menambahkan comment warna

--- while.js
+++ while.js
@@ -6,13 +6,13 @@ alert('tulisan telah dicetak pada console');
 var temp = 1;
 while (temp <= jumlahcetak) {
     console.log(tulisan + ' ke ' + temp);
-    temp = 1 + temp;
+    temp = 1 + temp; // * otomatis akan bertambah 1 nilai dari variabel temp
 
 }
 console.log('=================');
 var ulang = true;
-while (ulang) {
+while (ulang) { // !ini adalah perulangan value Defulth "true"
     console.log('Hello World!!');
-    ulang = confirm('ulangi kembali?');
-    console.log('terimakasih');
+    ulang = confirm('ulangi kembali?'); // ? ulangi atau tidak?
+    console.log('terimakasih'); // todo 
 }
