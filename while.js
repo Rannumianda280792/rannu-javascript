6ba292bfdbe140f50b210f404f4c0aa55df7cf56
rannumianda280792 <rannumianda92@gmail.com>
2020-03-02 13:45:41 +0800

    menambahkan komentar coba

--- while.js
+++ while.js
@@ -3,7 +3,7 @@ var tulisan = prompt('masukan tulisan yang di inginkan : '),
     cetak = confirm('cetak tulisan ' + tulisan + ' sebanyak ' + jumlahcetak + '?');
 alert('tulisan telah dicetak pada console');
 
-var temp = 1;
+var temp = 1; //!coba coba
 while (temp <= jumlahcetak) {
     console.log(tulisan + ' ke ' + temp);
     temp = 1 + temp; // * otomatis akan bertambah 1 nilai dari variabel temp
