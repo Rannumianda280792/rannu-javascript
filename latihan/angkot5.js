0f06aec5e88aa61d3e611872670c5ad9e6c96c06
rannumianda280792 <rannumianda92@gmail.com>
2020-03-03 15:44:29 +0800

    penambahan if bersarang

--- latihan/angkot5.js
+++ latihan/angkot5.js
@@ -1,9 +1,14 @@
 var banyak_angkot = prompt('input banyak angkot : ');
 for (var i = 1; i <= banyak_angkot; i++) {
     if (i <= 6) {
-        console.log('Angkot No. ' + i + ' Beroperasi dengan baik');
+        if (i === 5) { //! ===  memeriksa tipe variabel dan value yang sama
+            console.log('Angkot No. ' + i + ' sedang Lembur');
+        } else {
+            console.log('Angkot No. ' + i + ' Beroperasi dengan baik');
+        }
+
     } else if (i == 8 || i == banyak_angkot) {
-        console.log('Angkot No. ' + i + ' Lembur');
+        console.log('Angkot No. ' + i + ' sedang Lembur');
     } else {
         console.log('Angkot No. ' + i + ' Sedang tidak beroperasi');
     }
