1be659b2dc580a60d8e4cf8f24f6b9369345c512
rannumianda280792 <rannumianda92@gmail.com>
2020-03-04 10:10:21 +0800

    add page fuction.js and .html

---
+++ latihan/fuction.js
@@ -0,0 +1,13 @@
+function kali(a, b) {
+    let c = a * b;
+    var d = console.log('hasil ' + a + ' * ' + b + ' = ' + c);
+    d = c + 2;
+    return d, c, d;
+}
+
+function tambah(a, b) {
+    c = a + b;
+    return c;
+}
+var hasil = kali(2, 4);
+console.log(hasil);
